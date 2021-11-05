12170f48d650c3600e078028be543077407d816e
DangNT <dangnt520@wru.vn>
2021-11-06 04:28:08 +0700

    move actuator to sub dir

--- src/main/java/com/example/actuator/repository/ToDoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.actuator.repository;
-
-import com.example.actuator.entity.ToDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToDoRepository extends JpaRepository<ToDo, String> {
-    long countByCompleted(Boolean completed);
-}
